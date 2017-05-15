1c86d6dc6b47226d98ced3b36b0a13544b457e2f
fccoul <fccoul@gmail.com>
2017-05-15 15:01:03 +0000

    -Archivage Initial-

---
+++ myWeatherAppDemo/SignalR/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myWeatherAppDemo.SignalR
+{
+    public class ChatHub : Hub
+    {
+        static int _temperature;
+
+        [HubMethodName("Change_Weather")]
+        public void ChangeWeather(int temperature)
+        {
+            _temperature = temperature;
+            //NotifyUser is a function on the clientside, 
+            Clients.Others.NotifyUser(temperature);
+        }
+
+        [HubMethodName("get_weather")]//---methode depuis le client en JS (ts client se connectant au hub)
+        public void GetWeather()
+        {
+            //pushing the data only to the user which has called this method.   
+            Clients.Caller.NotifyUser(_temperature); //---led etail de cette methode sera defini coté JS sur lapage client
+        }
+    }
+}
---
+++ myWeatherAppDemo/SignalR/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly:OwinStartup(typeof(myWeatherAppDemo.SignalR.StartUp))]
+namespace myWeatherAppDemo.SignalR
+{
+    //We are calling owinstartup with the parameter WeatherAppDemo.SignalR.StartUp, which will initialize our StartUp class.
+    public class StartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Mapping : afin de pouvoir y acceder depuis l'url avec .../SignalR/hubs 
+            app.MapSignalR();
+        }
+    }
+}
